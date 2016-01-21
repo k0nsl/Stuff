fe54f849d76ec7532c8f16a2f551388d0007b30b
k0nsl <i.am@k0nsl.org>
2016-01-21 16:27:15 +0100

    Update client.php

--- Traffic-Monitor/client.php
+++ Traffic-Monitor/client.php
@@ -1,8 +1,6 @@
 <?php
-
 $device = "eth0";
 
 echo trim(file_get_contents("/sys/class/net/$device/statistics/rx_bytes")).":";
 echo trim(file_get_contents("/sys/class/net/$device/statistics/tx_bytes"))."";
-
 ?>
